6a5648493fad1b6e8d527f82b6bec56ade65eb19
asef18766 <asef18766@gmail.com>
2025-07-20 17:35:03 +0800

    finish data section encrytion scrpit

--- app/script/mod_section.py
+++ app/script/mod_section.py
@@ -1,9 +1,14 @@
 import lief
+from Crypto.Cipher import ChaCha20
+
 fname = "/workspaces/homo-os/build/zephyr/zephyr.elf"
 elf = lief.parse(fname)
 rodata:lief.ELF.Section = elf.get_section("rodata")
 # ref: https://www.sco.com/developers/gabi/latest/ch4.sheader.html
 SHF_WRITE = 0x1
 rodata.flags  |= SHF_WRITE
-print(len(rodata.content))
+key = 'やりますねやりますね\x00\x00'.encode()
+cipher = ChaCha20.new(key=key)
+ciphertext = cipher.encrypt(rodata.content)
+rodata.content = list(ciphertext)
 elf.write(fname)
