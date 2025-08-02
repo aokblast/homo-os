2f9cdfc4120d841990181ee64a98aa64f3586251
asef18766 <40972942+asef18766@users.noreply.github.com>
2025-08-02 17:41:42 +0800

    Feat/string enc (#9)
    * finish server prototype
    
    * add WIP string enc
    
    * add prototype of rodata decryption
    
    * finish data section encrytion scrpit
    
    * add WIP string enc
    
    * remove network dependency
    
    * fix lookup table & init nounce
    
    * add back server
    
    * fix stack size

---
+++ app/script/mod_section.py
@@ -0,0 +1,55 @@
+import lief
+from Crypto.Cipher import ChaCha20
+import sys
+def main(ifname:str, ofname:str):
+    elf = lief.ELF.parse(ifname)
+    rostart_va = -1
+    roend_va = -1
+    for sym in elf.symbols:
+        if sym.name == "__rodata_region_start":
+            rostart_va = sym.value
+        if sym.name == "__rodata_region_end":
+            roend_va = sym.value
+
+    for sec in elf.sections:
+        if sec.virtual_address == rostart_va:
+            rostart = sec.file_offset
+        
+        if sec.virtual_address + sec.size == roend_va:
+            roend = sec.file_offset + sec.size
+    
+    
+
+    print(f"ro start: {hex(rostart_va)} -> {hex(rostart)}")
+    print(f"ro end:   {hex(roend_va)  } -> {hex(roend)}")
+    
+    for idx, seg in enumerate(elf.segments):
+        if seg.file_offset <= rostart  and  roend <= seg.file_offset + len(seg.content):
+            seg.flags |= seg.FLAGS.W
+    elf.write(ofname)
+    
+    key = 'やりますねやりますね\x00\x00'.encode()
+    cipher = ChaCha20.new(key=key, nonce=b"\x00" * 12)
+
+    data = b""
+    with open(ofname, "rb") as fp:
+        data = fp.read()
+    pt = data[rostart:roend]
+    print(f"pt: {pt[:8].hex()}")
+    ct = cipher.encrypt(pt)
+    print(f"ct: {ct[:8].hex()}")
+    
+    data = data[:rostart] + ct + data[roend:]
+    print(f"data size: {hex(roend - rostart)}")
+    
+    with open(ofname, "wb") as fp:
+        fp.write(data)
+    print(f"success generate {ofname}")
+    
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print(sys.argv)
+        print(f'usage: {sys.argv[0]} <original elf> <output elf>')
+        exit(-1)
+    main(sys.argv[1], sys.argv[2])
